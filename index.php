f2cfcec3486e96f9db3a1ca81aaea70c9b7d5eb0
kda <2repby@gmail.com>
2023-03-15 14:25:16 +0500

    Task 1 Commit 15.03.2023

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require('dbconnect.php');
 
 
-$result = $conn->query("SELECT items.id as 'id', items.name as itn, categories.name as cn FROM items, categories WHERE categories.id=items.category_id");
+$result = $conn->query("SELECT items.id as 'id', items.name as itn, categories.name as cn FROM items, categories WHERE categories.id=items.id_category");
 while ($row = $result->fetch()) {
     echo('<p>'.$row['id'].' '.$row['itn'].' '.$row['cn'].'</p>');
     
